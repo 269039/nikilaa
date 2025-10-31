bd27f28a8b64645ef679d7bbe84676f2d7b3dd04
269039 <csbs269039@saranathan.ac.in>
2025-10-31 14:35:01 +0530

    Update s.py

--- s.py
+++ s.py
@@ -3,8 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World '
+def greet():
+    """Return a custom friendly HTTP greeting."""
+    return "Hello World!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app_host = "http://127.0.0.1:8080"
+    print(f"App host link: {app_host}")
+    app.run(host="127.0.0.1", port=8080, debug=True)
